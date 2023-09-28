23defd24667cce2a866c586786d197682f5ae2ce
tkoibaev <tam.koib12@gmail.com>
2023-09-28 22:13:15 +0300

    a6, mockapi used with fetch

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import PizzaCard from "./components/PizzaBlock/PizzaCard";
 
 import "./App.css";
 import "./scss/app.scss";
+import { useEffect, useState } from "react";
 
-import pizzas from "./assets/pizzas.json";
+// import pizzas from "./assets/pizzas.json";
 
 function App() {
+  //https://6515bedd09e3260018c917ce.mockapi.io/items - mockapi
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://6515bedd09e3260018c917ce.mockapi.io/items")
+      .then((responce) => responce.json())
+      .then((data) => setItems(data));
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -20,7 +30,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
+            {items.map((obj) => (
               <PizzaCard key={obj.id} {...obj} />
             ))}
           </div>
